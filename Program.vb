6ea8f91c09c77d509f943ba481341c8fdca34495
JustNrik <35231903+JustNrik@users.noreply.github.com>
2018-09-10 23:05:35 -0400

    Improved readability

--- Program.vb
+++ Program.vb
@@ -8,15 +8,24 @@ Module Program
     End Sub
 
     Async Function MainAsync() As Task
-        Dim serviceCollection As New ServiceCollection ' Initialise a ServiceCollection
-        serviceCollection.AddSingleton(New SqlConnection(ConnectionString)) ' Add a Singleton to each object you want to inject in your services
-        Dim services = AppDomain.CurrentDomain.GetAssemblies.SelectMany(Function(x) x.GetTypes).Where(Function(y) y.GetCustomAttributes(GetType(ServiceAttribute), True).Length > 0)
+        ' Initialise a ServiceCollection
+        Dim serviceCollection As New ServiceCollection 
+        ' Add a Singleton to each object you want to inject in your services
+        serviceCollection.AddSingleton(New SqlConnection(ConnectionString)) 
         ' I do this for ease, I can tag all my services with a custom attribute so their are easier to pick up
+        Dim services = AppDomain.CurrentDomain.GetAssemblies(). ' Gets the assemblies of your project
+            SelectMany(Function(x) x.GetTypes).
+            Where(Function(y) y.GetCustomAttributes(GetType(ServiceAttribute), True).Length > 0) ' This filters them for ServiceAttribute
+
         For Each service In services
             serviceCollection.AddSingleton(service) 'Otherwise you must add a singleton for each service manually
-        Next ' Like AddSingleton(Of StartupService).AddSingleton(Of DatabaseService).AddSingleton(Of TService).... and so on.
-        Dim servicePrivider = serviceCollection.BuildServiceProvider ' Here we have the provider, we've just injected out services and we are ready to use them.
-        Await servicePrivider.GetService(Of StartupService).InitialiseAsync ' Let's initialise them!
+        ' Like AddSingleton(Of StartupService).AddSingleton(Of DatabaseService).AddSingleton(Of TService).... and so on.
+        Next 
+            
+        ' Here we have the provider, we've just injected out services and we are ready to use them.
+        Dim servicePrivider = serviceCollection.BuildServiceProvider 
+        ' Let's initialise them!
+        Await servicePrivider.GetService(Of StartupService).InitialiseAsync 
     End Function
 
 End Module
