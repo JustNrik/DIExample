10ee25fdd46c66dd36b0dcc54f53f3524a147adb
JustNrik <35231903+JustNrik@users.noreply.github.com>
2018-09-10 23:08:06 -0400

    Added comment

--- Attributes/ServiceAttribute.vb
+++ Attributes/ServiceAttribute.vb
@@ -1,4 +1,5 @@
 ﻿<AttributeUsage(AttributeTargets.Class)>
 Public Class ServiceAttribute
     Inherits Attribute
+    ' Leave this class as it is, this serves only for filtering services.
 End Class
